4a66f30f319f3b86658d40b5f03ad50a5db9ee59
takara2314 <takara2314_TK@outlook.jp>
2022-02-18 16:16:34 +0900

    Add: Compass UI

--- lib/pages/race/navi.dart
+++ lib/pages/race/navi.dart
@@ -5,6 +5,7 @@ import 'package:sailing_assist_mie/providers/count.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:math';
 
 class RaceNavi extends HookConsumerWidget {
   const RaceNavi({Key? key}) : super(key: key);
@@ -34,74 +35,88 @@ class RaceNavi extends HookConsumerWidget {
       ),
       body: Column(
         children: [
-          SizedBox(
-            width: _width,
-            height: 350,
-            child: Stack(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(30),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle
-                  ),
-                )
-              ]
-            ),
-          ),
+          // SizedBox(
+          //   width: _width,
+          //   height: 350,
+          //   child: Stack(
+          //     children: [
+          //       // Container(
+          //       //   margin: const EdgeInsets.all(30),
+          //       //   decoration: const BoxDecoration(
+          //       //     color: Colors.white,
+          //       //     shape: BoxShape.circle
+          //       //   ),
+          //       // ),
+          //       SizedBox(
+          //         width: 200,
+          //         height: 200,
+          //         child: CustomPaint(
+          //           painter: _Compass()
+          //         )
+          //       )
+          //     ]
+          //   ),
+          // ),
           Container(
-            margin: const EdgeInsets.only(bottom: 30),
-            child: Column(
-              children: [
-                const Text(
-                  '② サイドマーク',
-                  style: TextStyle(
-                    fontSize: 32
-                  )
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 25, bottom: 25),
-                  child: const Text(
-                    '右',
-                    style: TextStyle(
-                      color: Color.fromRGBO(0, 94, 115, 1),
-                      fontWeight: FontWeight.w900,
-                      fontSize: 52
-                    )
-                  )
-                ),
-                Row(
-                  children: [
-                    const Text(
-                      '残り 約',
-                      style: TextStyle(
-                        color: Color.fromRGBO(79, 79, 79, 1),
-                        fontSize: 32
-                      )
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 10, right: 10),
-                      child: const Text(
-                        '17',
-                        style: TextStyle(
-                          color: Color.fromRGBO(79, 79, 79, 1),
-                          fontSize: 48
-                        )
-                      )
-                    ),
-                    const Text(
-                      'm',
-                      style: TextStyle(
-                        color: Color.fromRGBO(79, 79, 79, 1),
-                        fontSize: 32
-                      )
-                    )
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                )
-              ]
+            margin: const EdgeInsets.only(top: 30, bottom: 30),
+            child: SizedBox(
+              width: 300,
+              height: 300,
+              child: CustomPaint(
+                painter: _Compass()
+              )
             )
+          ),
+          Column(
+            children: [
+              const Text(
+                '② サイドマーク',
+                style: TextStyle(
+                  fontSize: 32
+                )
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 25, bottom: 25),
+                child: const Text(
+                  '右',
+                  style: TextStyle(
+                    color: Color.fromRGBO(0, 94, 115, 1),
+                    fontWeight: FontWeight.w900,
+                    fontSize: 52
+                  )
+                )
+              ),
+              Row(
+                children: [
+                  const Text(
+                    '残り 約',
+                    style: TextStyle(
+                      color: Color.fromRGBO(79, 79, 79, 1),
+                      fontSize: 32
+                    )
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    child: const Text(
+                      '17',
+                      style: TextStyle(
+                        color: Color.fromRGBO(79, 79, 79, 1),
+                        fontSize: 48
+                      )
+                    )
+                  ),
+                  const Text(
+                    'm',
+                    style: TextStyle(
+                      color: Color.fromRGBO(79, 79, 79, 1),
+                      fontSize: 32
+                    )
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+              )
+            ]
           )
         ]
       ),
@@ -114,14 +129,63 @@ class RaceNavi extends HookConsumerWidget {
 class _Compass extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    const lineLength = 30;
+    final startRadius = (size.width / 2) - lineLength - 10;
+    final endRadius = (size.width / 2) - 10;
+
     final paint = Paint();
-    paint.color = Colors.blue;
+    paint.color = Colors.white;
+    paint.strokeWidth = 5;
 
     canvas.drawCircle(
-      const Offset(100, 35),
-      25,
+      Offset(size.width / 2, size.height / 2),
+      size.width / 2,
       paint
     );
+
+    paint.color = const Color.fromRGBO(181, 181, 181, 1);
+
+    for (var theta = 0; theta < 360; theta+=30) {
+      canvas.drawLine(
+        Offset(
+          startRadius * cos(pi * theta / 180) + (size.width / 2),
+          startRadius * sin(pi * theta / 180) + (size.width / 2)
+        ),
+        Offset(
+          endRadius * cos(pi * theta / 180) + (size.width / 2),
+          endRadius * sin(pi * theta / 180) + (size.width / 2)
+        ),
+        paint
+      );
+    }
+
+    const direction = 90;
+
+    final path = Path();
+    path.moveTo(
+      startRadius * cos(pi * direction / 180) + (size.width / 2),
+      - startRadius * sin(pi * direction / 180) + (size.width / 2)
+    );
+
+    path.lineTo(
+      startRadius * cos(pi * (direction + 135) / 180) + (size.width / 2),
+      - startRadius * sin(pi * (direction + 135) / 180) + (size.width / 2)
+    );
+
+    path.lineTo(
+      startRadius * cos(pi * (direction + 225) / 180) + (size.width / 2),
+      - startRadius * sin(pi * (direction + 225) / 180) + (size.width / 2)
+    );
+
+    path.close();
+
+    // path.moveTo(0, 0);
+    // path.lineTo(10, 10);
+    // path.lineTo(0, 10);
+    // path.close();
+
+    paint.color = const Color.fromRGBO(0, 94, 115, 1);
+    canvas.drawPath(path, paint);
   }
 
   @override
