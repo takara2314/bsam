4c672f8c55938897fb2440176ee6a93cbb6ba0f7
takara2314 <takara2314_TK@outlook.jp>
2022-07-30 08:15:50 +0900

    Renew: v2 environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sailing_assist_mie/pages/home.dart';
+import 'package:bsam/pages/home.dart';
 
 void main() {
   runApp(const ProviderScope(child: App()));
@@ -14,38 +15,13 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'B-SAM',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.cyan,
         appBarTheme: const AppBarTheme(
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: 20
-          ),
-          centerTitle: false,
-          iconTheme: IconThemeData(
-            color: Color.fromRGBO(100, 100, 100, 1)
-          ),
-          elevation: 0,
-          backgroundColor: Colors.transparent
-        ),
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 18),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          hintStyle: TextStyle(color: Color.fromRGBO(137, 201, 223, 1)),
-          filled: true,
-          fillColor: Color.fromRGBO(209, 238, 248, 1),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10))
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            borderSide: BorderSide(color: Colors.transparent)
-          ),
-          contentPadding: EdgeInsets.only(top: 5, bottom: 5, left: 15, right: 15)
-        ),
-        backgroundColor: const Color.fromRGBO(229, 229, 229, 1)
+          systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        )
       ),
-      home: const Home()
+      home: const Home(),
     );
   }
 }
--- lib/providers.dart
+++ lib/providers.dart
@@ -1,3 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final userIdProvider = StateProvider((ref) => '00000');
+final userIdProvider = StateProvider((ref) => 'unknown');
