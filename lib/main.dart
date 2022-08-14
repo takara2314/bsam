8aca654077f6a4c45d7c024e8060a938025f887d
takara2314 <takara2314_TK@outlook.jp>
2022-08-14 22:50:34 +0900

    Improve: more m3 UI

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,53 @@ class App extends StatelessWidget {
         colorSchemeSeed: Colors.cyan,
         appBarTheme: const AppBarTheme(
           systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        ),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.primary
+          ),
+          headline2: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          headline3: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.tertiary,
+          ),
+          headline4: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+          ),
+          headline5: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold
+          ),
+          headline6: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.bold
+          ),
+          bodyText1: const TextStyle(
+            fontSize: 18
+          ),
+          bodyText2: const TextStyle(
+            fontSize: 16
+          ),
+          button: const TextStyle(
+            fontSize: 14
+          ),
+          caption: const TextStyle(
+            fontSize: 12
+          ),
+          overline: const TextStyle(
+            fontSize: 10
+          )
         )
       ),
-      home: const Home(),
+      home: const Home()
     );
   }
 }
--- lib/services/navi/compass.dart
+++ lib/services/navi/compass.dart
@@ -1,27 +1,27 @@
 String getDegName(double deg) {
   if (deg >= -22.5 && deg < 22.5) {
-    return '右';
+    return '前';
   }
   if (deg >= 22.5 && deg < 67.5) {
     return 'やや右';
   }
   if (deg >= 67.5 && deg < 112.5) {
-    return '前';
+    return '右';
   }
   if (deg >= 112.5 && deg < 157.5) {
-    return 'やや左';
+    return '右後ろ';
   }
   if (deg >= 157.5 || deg < -157.5) {
-    return '左';
+    return '下';
   }
   if (deg >= -157.5 && deg < -112.5) {
-    return '左後ろ';
+    return 'やや左';
   }
   if (deg >= -112.5 && deg < -67.5) {
-    return '後ろ';
+    return '左';
   }
   if (deg >= -67.5 && deg < -22.5) {
-    return '右後ろ';
+    return '左後ろ';
   }
 
   return '不明';
