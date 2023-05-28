ad9b266cdd7e5f604de1b3f6573cbb6cfd2316c8
takara2314 <takara2314_TK@outlook.jp>
2023-05-28 15:57:42 +0900

    Support: v4 API

--- lib/services/navi/mark.dart
+++ lib/services/navi/mark.dart
@@ -1,7 +1,7 @@
 import 'package:bsam/models/position.dart';
 
-List<Position> updateMarksOnEnable(List<Position> base, List<dynamic> received) {
-  final result = List.of(base);
+List<PositionWithId> updateMarksOnEnable(List<PositionWithId> current, List<dynamic> received) {
+  final result = List.of(current);
 
   for (int i = 0; i < received.length; i++) {
     if (received[i].lat == 0.0 && received[i].lng == 0.0) {
