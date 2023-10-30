f07a6f3583f87dda2f13d663e0335e9f1fd380fd
Takara Hamaguchi <takara2314_TK@outlook.jp>
2023-10-31 06:50:53 +0900

    Change: 角度と方向名の変換ロジックを変更

--- lib/services/navi/compass.dart
+++ lib/services/navi/compass.dart
@@ -1,8 +1,8 @@
 String getDegName(double deg) {
-  if (deg >= -30 && deg < 30) {
+  if (deg >= -15 && deg < 15) {
     return '前';
   }
-  if (deg >= 30 && deg < 60) {
+  if (deg >= 15 && deg < 60) {
     return 'やや右前';
   }
   if (deg >= 60 && deg < 120) {
@@ -20,7 +20,7 @@ String getDegName(double deg) {
   if (deg >= -120 && deg < -60) {
     return '左';
   }
-  if (deg >= -60 && deg < -30) {
+  if (deg >= -60 && deg < -15) {
     return 'やや左前';
   }
 
